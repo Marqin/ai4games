b135a550906d3b3decdb2856749cac5d1a5edbbc
Hubert Jarosz <marqin.pl@gmail.com>
2016-11-07 18:51:32 +0100

    add first version of CodingGame Tron Battle (Bronze)

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This is code for my university course called "AI For Games".
 Contents:
 
 * `labirynth` - solves [The Labirynth](https://www.codingame.com/training/hard/the-labyrinth) puzzle
+* `tron_battle` - plays [Tron Battle](https://www.codingame.com/multiplayer/bot-programming/tron-battle) game
---
+++ tron_battle/tron_battle.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2016 Hubert Jarosz
+#
+# This software is provided 'as-is', without any express or implied
+# warranty. In no event will the authors be held liable for any damages
+# arising from the use of this software.
+#
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+#
+# 1. The origin of this software must not be misrepresented; you must not
+#    claim that you wrote the original software. If you use this software
+#    in a product, an acknowledgement in the product documentation would be
+#    appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+#    misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+
+# currently at bronze
+
+import sys
+import math
+
+WIDTH = 30
+HEIGHT = 20
+
+################################################################################
+
+# checks if given coordinate is on map
+def is_on_map(x, y):
+    if x < 0 or x >= WIDTH or y < 0 or y >= HEIGHT:
+        return False
+    return True
+
+# gives map field neighbours that are on map
+def get_neighbours(x, y):
+    first_set = {(x-1, y), (x+1, y), (x, y-1), (x, y+1)}
+
+    return {(vx, vy) for (vx, vy) in first_set if is_on_map(vx, vy)}
+
+################################################################################
+
+class GameMap:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.map = [[-1]*self.height for n in range(self.width)]
+        #self.map = [[-1] * self.height] * self.width   ## BAD!!!!
+    def set(self, x, y, val=0):
+        self.map[x][y] = val
+    def get(self, x, y):
+        return self.map[x][y]
+
+    def freeNeighbours(self, x, y):
+        neibs = get_neighbours(x,y)
+        fn = [ (nx,ny) for (nx,ny) in neibs if self.get(nx,ny) < 0 ]
+        return fn
+
+################################################################################
+
+gameMap = GameMap(WIDTH, HEIGHT)
+
+# game loop
+while True:
+    # n: total number of players (2 to 4).
+    # p: your player number (0 to 3).
+    n, p = [int(i) for i in input().split()]
+    x, y = -1, -1
+    nbr = 0
+    for i in range(n):
+        x0, y0, x1, y1 = [int(j) for j in input().split()]
+
+        print(x0,y0,x1,y1, nbr, file=sys.stderr)
+
+        if x1 != -1:
+            gameMap.set(x1, y1, i+5)
+
+        if i == p:
+            x, y = x1, y1
+
+    if (x,y) == (-1,-1):
+        break
+
+    fn = gameMap.freeNeighbours(x,y)
+
+    if len(fn) == 0:
+        print("FAILURE!!!", file=sys.stderr)
+        print("DOWN")
+        continue
+
+    max_n = 0
+
+    for i in range(len(fn)):
+        curr = len(gameMap.freeNeighbours(fn[i][0],fn[i][1]))
+        best = len(gameMap.freeNeighbours(fn[max_n][0],fn[max_n][1]))
+        if curr > best:
+            max_n = i
+
+    for i in range(len(fn)):
+        curr = len(gameMap.freeNeighbours(fn[i][0],fn[i][1]))
+        best = len(gameMap.freeNeighbours(fn[max_n][0],fn[max_n][1]))
+        if curr < best and curr > 1:
+            max_n = i
+
+    go_to = fn[max_n]
+
+    if go_to[0] > x:
+        print("RIGHT")
+    elif go_to[0] < x:
+        print("LEFT")
+    elif go_to[1] > y:
+        print("DOWN")
+    else:
+        print("UP")
+    # To debug: print("Debug messages...", file=sys.stderr)
